4a56530f81b1d014f613bb356e3839c8223997f0
jesiahyesbis <110587876+jesiahyesbis@users.noreply.github.com>
2025-04-17 09:49:53 +0530

    Add files via upload

---
+++ Bank Management System/Tasks/NUnit Testing/CustomerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMBankDBConnect;
+
+namespace BankingTestProject1
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        public void EmailValidation_InvalidEmail_ThrowsArgumentException()
+        {
+            var customer = new Customer();
+            Assert.Throws<ArgumentException>(() =>
+            {
+                customer.EmailAddress = "invalid-email";
+            });
+        }
+    }
+}
