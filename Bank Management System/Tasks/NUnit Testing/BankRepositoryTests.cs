758342bc2064a91930bcaa275ec9b1c4174e8117
jesiahyesbis <110587876+jesiahyesbis@users.noreply.github.com>
2025-04-17 09:55:31 +0530

    Add files via upload

---
+++ Bank Management System/Tasks/NUnit Testing/BankRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMBankDBConnect;
+
+namespace BankingTestProject1
+{
+    [TestFixture]
+    public class BankRepositoryTests
+    {
+        private BankRepositoryImpl _repository;
+        private SqlConnection _testConnection;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testConnection = DBUtil.GetDBConn();
+            _repository = new BankRepositoryImpl();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _testConnection?.Close();
+        }
+
+        [Test]
+        public void Deposit_ValidAccount_UpdatesBalance()
+        {
+            // Arrange
+            long testAccountNumber = 1001; // Use existing test account
+            decimal initialBalance = _repository.GetAccountBalance(testAccountNumber);
+            decimal depositAmount = 500m;
+
+            // Act
+            decimal newBalance = _repository.Deposit(testAccountNumber, depositAmount);
+
+            // Assert
+            Assert.That(newBalance, Is.EqualTo(initialBalance + depositAmount));
+        }
+    }
+}
---
+++ Bank Management System/Tasks/NUnit Testing/SavingsAccountTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMBankDBConnect;
+
+namespace BankingTestProject1
+{
+    [TestFixture]
+    public class SavingsAccountTests
+    {
+        [Test]
+        public void Constructor_BalanceBelow500_ThrowsArgumentException()
+        {
+            var customer = new Customer(1, "John", "Doe", "john@test.com", "1234567890", "123 Main St");
+            Assert.That(() => new SavingsAccount(customer, 499, 4.0m),
+                Throws.ArgumentException.With.Message.Contains("minimum balance"));
+        }
+    }
+}
