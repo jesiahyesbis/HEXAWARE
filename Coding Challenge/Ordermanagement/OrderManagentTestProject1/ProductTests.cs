38d0c5e31c3be1b01ce16b07cd986b1485f2505b
jesiahyesbis <110587876+jesiahyesbis@users.noreply.github.com>
2025-04-17 14:20:29 +0530

    Add files via upload

---
+++ Coding Challenge/Ordermanagement/OrderManagentTestProject1/ProductTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagementSystem.entity;
+
+namespace OrderManagementTestProject
+{
+    [TestFixture]
+
+    public class ProductTests
+    {
+     
+
+            [Test]
+            public void Product_DefaultConstructor_InitializesProperties()
+            {
+                // Arrange // Act
+                var product = new Product();
+
+                // Assert
+                Assert.That(product.ProductId, Is.EqualTo(0));
+                Assert.That(product.ProductName, Is.Null);
+                Assert.That(product.Description, Is.Null);
+                Assert.That(product.Price, Is.EqualTo(0));
+                Assert.That(product.QuantityInStock, Is.EqualTo(0));
+                Assert.That(product.Type, Is.Null);
+            }
+    }
+}
+
+
---
+++ Coding Challenge/Ordermanagement/OrderManagentTestProject1/UserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagementSystem.entity;
+
+namespace OrderManagementTestProject
+{
+ 
+        [TestFixture]
+        public class UserTests
+        {
+            [Test]
+            public void User_Constructor_SetsProperties()
+            {
+                // Arrange //Act
+                var user = new User(1, "john_doe", "password123", "Admin");
+
+                // Assert
+                Assert.That(user.UserId, Is.EqualTo(1));
+                Assert.That(user.Username, Is.EqualTo("john_doe"));
+                Assert.That(user.Password, Is.EqualTo("password123"));
+                Assert.That(user.Role, Is.EqualTo("Admin"));
+            }
+        }
+    
+}
