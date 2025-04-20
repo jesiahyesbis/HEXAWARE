0ff8e36ab5418c58d1c98ca89ef87c9acad7fb30
jesiahyesbis <110587876+jesiahyesbis@users.noreply.github.com>
2025-04-21 00:11:47 +0530

    Add files via upload

---
+++ Case study new - Virtual Art Gallery/VArtGalleryTestsProject/ArtworkManagementTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualArtGalleryNew.DAO.Interfaces;
+using VirtualArtGalleryNew.DAO;
+using VirtualArtGalleryNew.Entities;
+using VirtualArtGalleryNew.Exceptions;
+using VirtualArtGalleryNew;
+
+namespace VArtGalleryTestProject
+{
+    [TestFixture]
+    public class ArtworkManagementTests
+    {
+        private ArtworkImpl artworkService;
+
+        [SetUp]
+        public void Setup()
+        {
+            artworkService = new ArtworkImpl();
+        }
+
+        [Test]
+        public void AddArtworkShouldAddArtworkWhenValidData()
+        {
+            var artwork = new Artwork
+            {
+                ArtworkID = 2001,
+                Title = "The Persistence of Memory",
+                Description = "A surreal painting by Salvador Dalí",
+                CreationDate = new DateTime(1931, 4, 1),
+                Medium = "Oil on canvas",
+                ImageURL = "https://example.com/dali.jpg",
+                ArtistID = 1
+            };
+
+            var result = artworkService.AddArtwork(artwork);
+            Assert.IsTrue(result);
+
+            // Cleanup
+            artworkService.RemoveArtwork(artwork.ArtworkID);
+        }
+
+        [Test]
+        public void UpdateArtworkShouldUpdateDetailsWhenArtworkExists()
+        {
+            var initialArtwork = new Artwork
+            {
+                ArtworkID = 2002,
+                Title = "Original Title",
+                Description = "Original Description",
+                CreationDate = DateTime.Now.AddYears(-10),
+                Medium = "Acrylic",
+                ImageURL = "https://original-image.com/image.jpg",
+                ArtistID = 1
+            };
+
+            artworkService.AddArtwork(initialArtwork);
+
+            var updatedArtwork = new Artwork
+            {
+                ArtworkID = 2002,
+                Title = "Updated Title",
+                Description = "Updated Description",
+                CreationDate = DateTime.Now,
+                Medium = "Updated Medium",
+                ImageURL = "https://updated-image.com/image.jpg",
+                ArtistID = 1
+            };
+
+            var result = artworkService.UpdateArtwork(updatedArtwork);
+            Assert.IsTrue(result);
+
+            // Cleanup
+            artworkService.RemoveArtwork(updatedArtwork.ArtworkID);
+        }
+
+        [Test]
+        public void RemoveArtworkShouldDeleteArtworkWhenArtworkExists()
+        {
+            var artwork = new Artwork
+            {
+                ArtworkID = 2003,
+                Title = "To Be Deleted",
+                Description = "This will be removed",
+                CreationDate = DateTime.Now,
+                Medium = "Mixed media",
+                ImageURL = "https://example.com/delete.jpg",
+                ArtistID = 1
+            };
+
+            artworkService.AddArtwork(artwork);
+            var result = artworkService.RemoveArtwork(artwork.ArtworkID);
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void SearchArtworkShouldReturnMatchingResultsWhenKeywordExists()
+        {
+            var artwork = new Artwork
+            {
+                ArtworkID = 2004,
+                Title = "Sunset in Venice",
+                Description = "A stunning sunset over canals",
+                CreationDate = DateTime.Now,
+                Medium = "Oil",
+                ImageURL = "https://example.com/sunset.jpg",
+                ArtistID = 1
+            };
+
+            artworkService.AddArtwork(artwork);
+
+            string keyword = "Sunset";
+            List<Artwork> results = artworkService.SearchArtwork(keyword);
+
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.Count > 0);
+            Assert.IsTrue(results.Exists(a => a.Title.Contains("Sunset") || a.Description.Contains("Sunset")));
+
+            // Cleanup
+            artworkService.RemoveArtwork(artwork.ArtworkID);
+        }
+
+        [Test]
+        public void AddArtworkShouldThrowExceptionWhenArtworkIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => artworkService.AddArtwork(null));
+        }
+
+        [Test]
+        public void UpdateArtworkShouldThrowExceptionWhenArtworkDoesNotExist()
+        {
+            var artwork = new Artwork
+            {
+                ArtworkID = 9999,
+                Title = "Ghost Artwork",
+                Description = "Does not exist",
+                CreationDate = DateTime.Now,
+                Medium = "Ink",
+                ImageURL = "https://example.com/ghost.jpg",
+                ArtistID = 1
+            };
+
+            Assert.Throws<ArtworkNotFoundException>(() => artworkService.UpdateArtwork(artwork));
+        }
+
+        [Test]
+        public void RemoveArtworkShouldThrowExceptionWhenArtworkNotFound()
+        {
+            Assert.Throws<ArtworkNotFoundException>(() => artworkService.RemoveArtwork(9999));
+        }
+
+        [Test]
+        public void SearchArtworkShouldThrowExceptionWhenKeywordIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => artworkService.SearchArtwork(""));
+        }
+    }
+}
---
+++ Case study new - Virtual Art Gallery/VArtGalleryTestsProject/GalleryManagementTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualArtGalleryNew.DAO;
+using VirtualArtGalleryNew.DAO.Interfaces;
+using VirtualArtGalleryNew.Entities;
+using VirtualArtGalleryNew.Exceptions;
+using VirtualArtGalleryNew;
+
+namespace VArtGalleryTestProject
+{
+    [TestFixture]
+    public class GalleryManagementTests
+    {
+        private GalleryImpl galleryService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            galleryService = new GalleryImpl();
+        }
+
+        [Test]
+        public void AddGalleryShouldAddGalleryWhenValidData()
+        {
+            // Arrange
+            var gallery = new Gallery(30, "Test Gallery1", "Test Description1", "Test Location1", 1, "9AM-5PM");
+
+            // Act
+            var result = galleryService.AddGallery(gallery);
+
+            // Assert
+            Assert.IsTrue(result);
+
+            // Cleanup
+            galleryService.RemoveGallery(gallery.GalleryID);
+        }
+
+        [Test]
+        public void UpdateGalleryShouldUpdateFieldsWhenValidData()
+        {
+            // Arrange
+            var gallery = new Gallery(29, "Initial Name", "Initial Description", "Initial Location", 1, "10AM-6PM");
+            galleryService.AddGallery(gallery);
+
+            gallery.Name = "Updated Name";
+            gallery.Description = "Updated Description";
+            gallery.Location = "Updated Location";
+
+            // Act
+            var result = galleryService.UpdateGallery(gallery);
+
+            // Assert
+            Assert.IsTrue(result);
+            var updatedGallery = galleryService.GetGalleryById(29);
+            Assert.AreEqual("Updated Name", updatedGallery.Name);
+
+            // Cleanup
+            galleryService.RemoveGallery(29);
+        }
+
+        [Test]
+        public void RemoveGalleryShouldDeleteWhenGalleryExists()
+        {
+            // Arrange
+            var gallery = new Gallery(28, "To Delete", "Desc", "Loc", 1, "11AM-4PM");
+            galleryService.AddGallery(gallery);
+
+            // Act
+            var result = galleryService.RemoveGallery(28);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.Throws<GalleryNotFoundException>(() => galleryService.GetGalleryById(28));
+        }
+
+        [Test]
+        public void SearchGalleriesShouldReturnResultsWhenKeywordMatches()
+        {
+            // Arrange
+            var gallery = new Gallery(27, "Keyword Gallery", "Awesome art place", "New York", 1, "9AM-6PM");
+            galleryService.AddGallery(gallery);
+
+            // Act
+            var results = galleryService.SearchGalleries("Keyword");
+
+            // Assert
+            Assert.IsTrue(results.Exists(g => g.GalleryID == 27));
+
+            // Cleanup
+            galleryService.RemoveGallery(27);
+        }
+
+        [Test]
+        public void AddGalleryShouldThrowWhenDuplicateID()
+        {
+            // Arrange
+            var gallery = new Gallery(26, "Duplicate Test", "Desc", "Loc", 1, "10AM-5PM");
+            galleryService.AddGallery(gallery);
+
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentException>(() => galleryService.AddGallery(gallery));
+            Assert.That(ex.Message, Does.Contain("already exists"));
+
+            // Cleanup
+            galleryService.RemoveGallery(26);
+        }
+
+        [Test]
+        public void SearchGalleriesShouldThrowWhenKeywordIsNull()
+        {
+            Assert.Throws<ArgumentException>(() => galleryService.SearchGalleries(null));
+        }
+
+
+    }
+}
